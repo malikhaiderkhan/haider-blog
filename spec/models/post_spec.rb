26d01a6a1e9ed658a2616e5bbf3805e5d3b865e9
Haider <malikhaiderkhan81@gmail.com>
2023-10-28 16:32:03 +0500

    add describe for the tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,55 +7,59 @@ RSpec.describe Post, type: :model do
 
   before { subject.save }
 
-  it 'title should be present' do
-    subject.title = nil
-    expect(subject).to_not be_valid
+  describe 'validations' do
+    it 'title should be present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'title should be less than or equal to 250' do
+      subject.title = 'a' * 300
+      expect(subject).to_not be_valid
+    end
+
+    it 'comments_counter should be non negative integer' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+
+      subject.comments_counter = 0
+      expect(subject).to be_valid
+
+      subject.comments_counter = 5
+      expect(subject).to be_valid
+    end
+
+    it 'likes_counter should be non negative integer' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+
+      subject.likes_counter = 0
+      expect(subject).to be_valid
+
+      subject.likes_counter = 5
+      expect(subject).to be_valid
+    end
   end
 
-  it 'title should be less than or equal to 250' do
-    subject.title = 'a' * 300
-    expect(subject).to_not be_valid
-  end
+  describe 'assocaiations' do
+    it 'should update posts counter' do
+      Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+      Post.create(author: first_user, title: 'Hello', text: 'This is my 2nd post')
+      expect(first_user.posts_counter).to eq(3)
+    end
 
-  it 'comments_counter should be non negative integer' do
-    subject.comments_counter = -1
-    expect(subject).to_not be_valid
+    it 'should return five recent comments' do
+      subject.save
+      subject.comments.create(user: first_user, text: 'Comment 1')
+      comment2 = subject.comments.create(user: first_user, text: 'Comment 2')
+      comment3 = subject.comments.create(user: first_user, text: 'Comment 3')
+      comment4 = subject.comments.create(user: first_user, text: 'Comment 4')
+      comment5 = subject.comments.create(user: first_user, text: 'Comment 5')
+      comment6 = subject.comments.create(user: first_user, text: 'Comment 6')
 
-    subject.comments_counter = 0
-    expect(subject).to be_valid
+      recent_comments = subject.five_recent_comments
 
-    subject.comments_counter = 5
-    expect(subject).to be_valid
-  end
-
-  it 'likes_counter should be non negative integer' do
-    subject.likes_counter = -1
-    expect(subject).to_not be_valid
-
-    subject.likes_counter = 0
-    expect(subject).to be_valid
-
-    subject.likes_counter = 5
-    expect(subject).to be_valid
-  end
-
-  it 'should update posts counter' do
-    Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-    Post.create(author: first_user, title: 'Hello', text: 'This is my 2nd post')
-    expect(first_user.posts_counter).to eq(3)
-  end
-
-  it 'should return five recent comments' do
-    subject.save
-    subject.comments.create(user: first_user, text: 'Comment 1')
-    comment2 = subject.comments.create(user: first_user, text: 'Comment 2')
-    comment3 = subject.comments.create(user: first_user, text: 'Comment 3')
-    comment4 = subject.comments.create(user: first_user, text: 'Comment 4')
-    comment5 = subject.comments.create(user: first_user, text: 'Comment 5')
-    comment6 = subject.comments.create(user: first_user, text: 'Comment 6')
-
-    recent_comments = subject.five_recent_comments
-
-    expect(recent_comments).to eq([comment6, comment5, comment4, comment3, comment2])
+      expect(recent_comments).to eq([comment6, comment5, comment4, comment3, comment2])
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,26 +5,30 @@ RSpec.describe User, type: :model do
 
   before { subject.save }
 
-  it 'name should be present' do
-    user = User.create(photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    expect(user).to_not be_valid
+  describe 'validations' do
+    it 'name should be present' do
+      user = User.create(photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      expect(user).to_not be_valid
+    end
+
+    it 'post_count should be non negative integer' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+
+      subject.posts_counter = 0
+      expect(subject).to be_valid
+
+      subject.posts_counter = 5
+      expect(subject).to be_valid
+
+      subject.posts_counter = 'abc'
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'post_count should be non negative integer' do
-    subject.posts_counter = -1
-    expect(subject).to_not be_valid
-
-    subject.posts_counter = 0
-    expect(subject).to be_valid
-
-    subject.posts_counter = 5
-    expect(subject).to be_valid
-
-    subject.posts_counter = 'abc'
-    expect(subject).to_not be_valid
-  end
-
-  it 'expect recent posts to be 0 ' do
-    expect(subject.three_recent_posts.length).to eq(0)
+  describe 'recent posts' do
+    it 'expect recent posts to be 0 ' do
+      expect(subject.three_recent_posts.length).to eq(0)
+    end
   end
 end
