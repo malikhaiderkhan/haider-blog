89ec201319aff5cdab2d9aef8d7d68b085f491f2
Haider <malikhaiderkhan81@gmail.com>
2023-11-09 02:06:32 +0500

    add bootstrap styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @user = User.find_by(id: params[:user_id])
+
+    if @user
+      @posts = @user.posts.page(params[:page]).per(10)
+    else
+      flash[:alert] = "User not found."
+      redirect_to root_path
+    end
   end
 
   # GET /posts/1 or /posts/1.json
