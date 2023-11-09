5026f113f2612eb05827f2be3b9e3e867818a469
Haider <malikhaiderkhan81@gmail.com>
2023-11-10 01:15:25 +0500

    add all styling and features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,13 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @user = User.find_by(id: params[:user_id])
-
-    if @user
-      @posts = @user.posts.page(params[:page]).per(10)
-    else
-      flash[:alert] = "User not found."
-      redirect_to root_path
-    end
+    @posts = Post.where(author_id: @user.id)
   end
 
   # GET /posts/1 or /posts/1.json
   def show
     @post = Post.find(params[:id])
-    @posts = @post.author.posts
+    @index = params[:index]
     @post_index = @post.author.posts.order(created_at: :desc).pluck(:id).index(@post.id) + 1
   end
 end
