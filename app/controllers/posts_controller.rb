184b2a746f46549eaae41d4d33217707e8bbf939
Haider <malikhaiderkhan81@gmail.com>
2023-11-14 23:26:46 +0500

    update index action of posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @user = User.find_by(id: params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+    @posts = Post.includes(:comments).where(author_id: @user.id)
   end
 
   # GET /posts/1 or /posts/1.json
