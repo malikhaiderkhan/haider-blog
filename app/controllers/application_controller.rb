b3d6b322d5cba8c26bb3f4eaed4ef6956085419b
DevaWinner <winneraaniekan@gmail.com>
2023-11-17 16:56:12 +0100

    Add update_allowed_parameters method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  #add here
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+ end
 end
